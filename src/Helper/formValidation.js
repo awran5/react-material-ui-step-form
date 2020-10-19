0e2c93e47bf4f90abb7567de3bd7b807a60cc979
awran5 <awran5@yahoo.com>
2020-10-19 22:53:35 +0200

    typo

--- src/Helper/formValidation.js
+++ src/Helper/formValidation.js
@@ -8,21 +8,21 @@ export default function formValidation(name, value, schema) {
   const { validate, minLength, maxLength } = schema[name]
   let error = ""
 
-  if (minLength && value.length < minLength) error = `Minimum ${minLength} characters is required.`
+  if (minLength && value.length < minLength) error = `Minimum of ${minLength} characters are required.`
   else if (maxLength && value.length > maxLength) error = `Maximum length of ${maxLength} exceeded!`
   if (!validate) return
 
   switch (validate) {
     case "text":
-      if (!isText.test(value)) error = "This field accept text only."
+      if (!isText.test(value)) error = "This field accepts text only."
       break
 
     case "number":
-      if (!isNumber.test(value)) error = "This field accept numbers only."
+      if (!isNumber.test(value)) error = "This field accepts numbers only."
       break
 
     case "email":
-      if (!isEmail.test(value)) error = "Please enter a valid email"
+      if (!isEmail.test(value)) error = "Please enter a valid email address."
       break
 
     case "phone":
@@ -30,11 +30,11 @@ export default function formValidation(name, value, schema) {
       break
 
     case "zip":
-      if (!isZip.test(value)) error = "Please enter a valid zip code"
+      if (!isZip.test(value)) error = "Please enter a valid zip code."
       break
 
     case "checkbox":
-      if (!value) error = "Please select a value"
+      if (!value) error = "Please select a value."
       break
 
     default:
